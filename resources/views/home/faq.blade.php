f13cfbe1950397dbf8cc0a1cca34d22a0a3198bc
Mohsen <shaiwoe@gmail.com>
2023-05-13 21:57:08 +0330

    faq fix

--- resources/views/home/faq.blade.php
+++ resources/views/home/faq.blade.php
@@ -33,7 +33,7 @@
                         class="flex items-center justify-between w-full p-5 text-xs lg:text-base text-white dark:text-gray-700 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 "
                         data-accordion-target="#accordion-open-body-1" aria-expanded="false"
                         aria-controls="accordion-open-body-1">
-                        <span class="flex items-center text-black dark:text-white"><svg class="w-5 h-5 mr-2 shrink-0" fill="currentColor"
+                        <span class="flex items-center text-black dark:text-gray-500"><svg class="w-5 h-5 mr-2 shrink-0" fill="currentColor"
                                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd"
                                     d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
@@ -49,7 +49,7 @@
                 </h2>
                 <div id="accordion-open-body-1" class="hidden" aria-labelledby="accordion-open-heading-1">
                     <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
-                        <p class="mb-2 text-gray-100 dark:text-gray-200">
+                        <p class="mb-2 text-gray-100 dark:text-gray-700">
                             مجموعه آلفارنسی سعی دارد تا با برگزاری سمینارها ، وبینارها و تولیدمحتواهای آموزشی (مقاله ،
                             پادکست ، کتاب و ویدئو) در بسترهای مختلف، مسیر آموزش شما عزیزان را نیز هموار سازد .
                         </p>
@@ -61,7 +61,7 @@
                         class="flex items-center justify-between w-full p-5 text-xs lg:text-base text-white dark:text-gray-700 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 "
                         data-accordion-target="#accordion-open-body-2" aria-expanded="false"
                         aria-controls="accordion-open-body-2">
-                        <span class="flex items-center text-black dark:text-white"><svg class="w-5 h-5 mr-2 shrink-0" fill="currentColor"
+                        <span class="flex items-center text-black dark:text-gray-500"><svg class="w-5 h-5 mr-2 shrink-0" fill="currentColor"
                                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd"
                                     d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
@@ -91,7 +91,7 @@
                         class="flex items-center justify-between w-full p-5 text-xs lg:text-base text-white dark:text-gray-700 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700"
                         data-accordion-target="#accordion-open-body-3" aria-expanded="false"
                         aria-controls="accordion-open-body-3">
-                        <span class="flex items-center text-black dark:text-white"><svg class="w-5 h-5 mr-2 shrink-0" fill="currentColor"
+                        <span class="flex items-center text-black dark:text-gray-500"><svg class="w-5 h-5 mr-2 shrink-0" fill="currentColor"
                                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd"
                                     d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
@@ -121,7 +121,7 @@
                         class="flex items-center justify-between w-full p-5 text-xs lg:text-base text-white dark:text-gray-700 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 "
                         data-accordion-target="#accordion-open-body-4" aria-expanded="false"
                         aria-controls="accordion-open-body-4">
-                        <span class="flex items-center text-black dark:text-white"><svg class="w-5 h-5 mr-2 shrink-0" fill="currentColor"
+                        <span class="flex items-center text-black dark:text-gray-500"><svg class="w-5 h-5 mr-2 shrink-0" fill="currentColor"
                                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd"
                                     d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
@@ -152,7 +152,7 @@
                         class="flex items-center justify-between w-full p-5 text-xs lg:text-base text-white dark:text-gray-700 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 "
                         data-accordion-target="#accordion-open-body-5" aria-expanded="false"
                         aria-controls="accordion-open-body-5">
-                        <span class="flex items-center text-black dark:text-white"><svg class="w-5 h-5 mr-2 shrink-0" fill="currentColor"
+                        <span class="flex items-center text-black dark:text-gray-500"><svg class="w-5 h-5 mr-2 shrink-0" fill="currentColor"
                                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd"
                                     d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
@@ -182,7 +182,7 @@
                         class="flex items-center justify-between w-full p-5 text-xs lg:text-base text-white dark:text-gray-700 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 "
                         data-accordion-target="#accordion-open-body-6" aria-expanded="false"
                         aria-controls="accordion-open-body-6">
-                        <span class="flex items-center text-black dark:text-white"><svg class="w-5 h-5 mr-2 shrink-0" fill="currentColor"
+                        <span class="flex items-center text-black dark:text-gray-500"><svg class="w-5 h-5 mr-2 shrink-0" fill="currentColor"
                                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd"
                                     d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
@@ -213,7 +213,7 @@
                         class="flex items-center justify-between w-full p-5 text-xs lg:text-base text-white dark:text-gray-700 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700  dark:hover:bg-gray-800"
                         data-accordion-target="#accordion-open-body-7" aria-expanded="false"
                         aria-controls="accordion-open-body-7">
-                        <span class="flex items-center text-black dark:text-white"><svg class="w-5 h-5 mr-2 shrink-0" fill="currentColor"
+                        <span class="flex items-center text-black dark:text-gray-500"><svg class="w-5 h-5 mr-2 shrink-0" fill="currentColor"
                                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd"
                                     d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
