286af3af32747f4ebbc2f5d593f750dfa5d33641
mohamad parvizi <mohammadpp955.pp955@gmail.com>
2023-05-18 18:54:49 +0330

    front edit
    contant
    about
    faq
    footer

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -44,25 +44,65 @@
 
 
 
-    <div class="w-full container flex mx-auto text-white dark:text-gray-700 z-30 p-4">
-        <form class="flex flex-col bg-coin1 dark:bg-white w-full gap-12 p-4 lg:p-12 z-30 mt-28 lg:mt-56" id="coinBox" action="">
+    <div class="full-width container mx-auto p-4 lg:p-8 text-white z-50 ">
+        <div class=" flex flex-col lg:flex-row mt-28 lg:mt-30">
+            <div class="flex-1">
+
+                <div class="w-full xl:w-4/6  grid grid-cols-2 gap-10 z-50">
+                    <a  id="coinBox" href="https://www.instagram.com/alpharency/" class="flex flex-col gap-3 justify-center items-center py-10 bg-coin1 dark:bg-purple1">
+                        <img id="" class="p-2 rounded-md w-20 h-20" src="image/svg/instagram.svg" alt="">
+                        <p>Instagram</p>
+                    </a>
+
+                    <a id="coinBox" href="https://t.me/alpharency" class="flex flex-col gap-3 justify-center items-center py-10 bg-coin1 dark:bg-purple1">
+                        <img id="" class="p-2 rounded-md w-20 h-20" src="image/svg/send.svg" alt="">
+                        <p>Telegram</p>
+                    </a>
+
+                    <a id="coinBox" href="https://www.youtube.com/@alpharency" class="flex flex-col gap-3 justify-center items-center py-10 bg-coin1 dark:bg-purple1">
+                        <img id="" class="p-2 rounded-md w-20 h-20" src="image/svg/youtube.svg" alt="">
+                        <p>Twitter</p>
+                    </a>
+
+                    <a id="coinBox" href="https://twitter.com/alpharency" class="flex flex-col gap-3 justify-center items-center py-10 bg-coin1 dark:bg-purple1">
+                        <img id="" class="p-2 rounded-md w-20 h-20" src="image/svg/twitter.svg" alt="">
+                        <p>Yourube</p>
+                    </a>
+                </div>
+            </div>
+            <div class="flex-1 text-center z-30 relative self-center" style="text-align: -webkit-center;">
+                <p class="text-center text-2xl z-30 xl:absolute top-0 left-0 my-8 xl:my-0">شبکه های اجتماعی ما را هم دنبال کنید</p>
+                <img src="image/svg/contact-svg.svg" class="w-8/12" alt="">
+            </div>
+        </div>
+
+
+
+    </div>
+
+
+
+
+
+    <div class="container flex mx-auto text-white dark:text-gray-700 z-30 p-4 lg:p-8">
+        <form class="flex flex-col bg-coin1 dark:bg-white  dark:shadow-2xl w-full gap-12 p-4 lg:p-12 z-30" id="coinBox" action="">
             <p class="lg:text-2xl">برای ارتباط با ما پیام دهید</p>
             <div class="flex flex-col w-full gap-12 z-50">
                 <div class="flex flex-col lg:flex-row w-full gap-12 z-50 text-white dark:text-gray-700">
                     <div class="flex flex-col w-full z-50">
                         <label class="text-sm lg:text-base" for="">نام و نام خانوادگی</label>
-                        <input id="contact-input" class="mt-2" type="text" name="" id="">
+                        <input id="contact-input" class="mt-2 contact-input dark:bg-icon-light" type="text" name="" id="">
 
                         <label class="mt-9 text-sm lg:text-base" for="">آدرس ایمیل</label>
-                        <input id="contact-input" class="mt-2" type="text" name="" id="">
+                        <input id="contact-input" class="mt-2 contact-input dark:bg-icon-light" type="text" name="" id="">
 
                         <label class="mt-9 text-sm lg:text-base" for="">عنوان تیکت</label>
-                        <input id="contact-input" class="mt-2" type="text" name="" id="">
+                        <input id="contact-input" class="mt-2 contact-input dark:bg-icon-light" type="text" name="" id="">
                     </div>
 
                     <div class="flex flex-col w-full">
                         <label for="">متن تیکت</label>
-                        <textarea id="contact-input" class="mt-4" name="" id="" cols="30" rows="11"></textarea>
+                        <textarea id="contact-input" class="mt-4 contact-input dark:bg-icon-light" name="" id="" cols="30" rows="11"></textarea>
                     </div>
                 </div>
 
@@ -74,31 +114,7 @@
     </div>
 
 
-    <div class="flex flex-col container mx-auto p-4 lg:p-8 text-white dark:text-gray-700 z-50">
-        <p class="text-center lg:text-2xl z-30 mt-12">شبکه های اجتماعی ما را هم دنبال کنید</p>
-        <div id="coinBox" class="grid grid-cols-2 lg:grid-cols-4 gap-8 z-50 p-8 mt-8 bg-coin1 dark:bg-white ">
 
-            <a href="https://www.instagram.com/alpharency/" class="flex flex-col gap-3 justify-center items-center">
-                <img id="contact-icon" class="p-2 rounded-md w-14 h-14" src="image/svg/instagram.svg" alt="">
-                <p>instagram</p>
-            </a>
-
-            <a href="https://t.me/alpharency" class="flex flex-col gap-3 justify-center items-center">
-                <img id="contact-icon" class="p-2 rounded-md w-14 h-14" src="image/svg/send.svg" alt="">
-                <p>telegram</p>
-            </a>
-
-            <a href="https://www.youtube.com/@alpharency" class="flex flex-col gap-3 justify-center items-center">
-                <img id="contact-icon" class="p-2 rounded-md w-14 h-14" src="image/svg/youtube.svg" alt="">
-                <p>twitter</p>
-            </a>
-
-            <a href="https://twitter.com/alpharency" class="flex flex-col gap-3 justify-center items-center">
-                <img id="contact-icon" class="p-2 rounded-md w-14 h-14" src="image/svg/twitter.svg" alt="">
-                <p>yourube</p>
-            </a>
-        </div>
-    </div>
 
 
     <!-- top footer  -->
