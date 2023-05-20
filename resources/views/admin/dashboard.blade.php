abd1caa02598e99a7dbca8c23f703101f7bed17c
Mohsen <shaiwoe@gmail.com>
2023-05-21 02:59:36 +0330

    panel dashbord18

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -57,9 +57,9 @@
             <div class="w-full grid grid-cols-4 gap-4 bg-coin1 p-4" id="coinBox">
                 @foreach ($articless as $article)
                     <div class="flex w-full bg-coin1" id="coinBox">
-                        <div class="flex gap-8 justify-center items-center text-center flex-col space-y-4 w-full">
+                        <div class="flex  justify-center items-center text-center flex-col space-y-4 w-full">
                             <a href="{{ route('home.articles.show', ['article' => $article->slug]) }}">
-                                <img class="rounded-t-xl w-full h-36"
+                                <img class="rounded-t-xl w-full h-44"
                                     src="{{ asset(env('ARTICLES_IMAGES_UPLOAD_PATH') . $article->primary_image) }}"
                                     alt="">
                             </a>
