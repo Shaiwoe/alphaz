4871f900f0425d70a8ca38a66b4363bf0791567b
Mohsen <shaiwoe@gmail.com>
2023-05-22 03:51:10 +0330

    build3

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -88,7 +88,7 @@
                         </div>
 
                         <div class="flex gap-4 mt-14">
-                            <button type="submit" class="bg-yellow-400 hover:bg-yellow-500 w-full lg:w-1/12 p-2 rounded-lg  text-gray-800 ">ثبت</button>
+                            <button type="submit" class="bg-green-400 hover:bg-green-500 w-full lg:w-1/12 p-2 rounded-lg  text-gray-100 ">ثبت</button>
                             <a href="{{ route('users.index') }}" class="bg-red-500 text-center hover:bg-red-600 w-full lg:w-1/12 p-2  rounded-lg text-white">بازگشت</a>
                         </div>
 
