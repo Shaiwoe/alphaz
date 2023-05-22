70b472df54c0258b8f4b01e97fa54632c95b308b
Mohsen <shaiwoe@gmail.com>
2023-05-22 04:52:04 +0330

    dashbord

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -17,7 +17,18 @@
     {{-- header  --}}
     @include('components/header')
 
-    @include('components/light')
+    <div class="light dark:opacity-40 relative w-full">
+        <div class="absolute top-0 left-0">
+            <img src="/image/tinified/1.png" alt="">
+        </div>
+    </div>
+
+
+    <div class="light dark:opacity-40 relative w-full">
+        <div class="absolute top-[100%] right-0">
+            <img src="/image/tinified/2.png" alt="">
+        </div>
+    </div>
 
 
     <div class="flex justify-between  relative space-x-4">
