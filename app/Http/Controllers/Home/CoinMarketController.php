b5785b84121777b7ea7027d113d6b5201166973a
Saeed Johari <persgeek@gmail.com>
2023-05-31 12:06:16 +0330

    Improve list of coins

--- app/Http/Controllers/Home/CoinMarketController.php
+++ app/Http/Controllers/Home/CoinMarketController.php
@@ -8,6 +8,12 @@ class CoinMarketController extends Controller
 {
     public function list()
     {
+        $price = $this->getUSDTPrice();
+
+        // Sell price
+        $sellPrice = array_get($price, 'stats.usdt-rls.bestSell');
+
+        // List coins
         $response = $this->getMarketCoins();
 
         // Check response
@@ -16,7 +22,7 @@ class CoinMarketController extends Controller
 
         foreach ($response->data as $coin) {
 
-            $coins[] = (object) ['name' => $coin->name, 'symbol' => $coin->symbol, 'price' => $coin->quote->USD->price, 'volume_24h' => $coin->quote->USD->volume_24h, 'percent_change_1h' => $coin->quote->USD->percent_change_1h, 'percent_change_24h' => $coin->quote->USD->percent_change_24h, 'percent_change_7d' => $coin->quote->USD->percent_change_7d, 'market_cap' => $coin->quote->USD->market_cap];
+            $coins[] = (object) ['name' => $coin->name, 'symbol' => $coin->symbol, 'price' => $coin->quote->USD->price, 'price_rls' => ($coin->quote->USD->price * $sellPrice), 'volume_24h' => $coin->quote->USD->volume_24h, 'percent_change_1h' => $coin->quote->USD->percent_change_1h, 'percent_change_24h' => $coin->quote->USD->percent_change_24h, 'percent_change_7d' => $coin->quote->USD->percent_change_7d, 'market_cap' => $coin->quote->USD->market_cap];
         }
 
         return view('home.coins.index', compact('coins'));
@@ -24,6 +30,12 @@ class CoinMarketController extends Controller
 
     public function show($symbol)
     {
+        $price = $this->getUSDTPrice();
+
+        // Sell price
+        $sellPrice = array_get($price, 'stats.usdt-rls.bestSell');
+
+        // List coins
         $response = $this->getMarketCoin($symbol);
 
         // Check response
@@ -32,10 +44,24 @@ class CoinMarketController extends Controller
 
         foreach ($response->data as $coin) {
 
-            $coin = (object) ['name' => $coin->name, 'symbol' => $coin->symbol, 'price' => $coin->quote->USD->price, 'volume_24h' => $coin->quote->USD->volume_24h, 'percent_change_1h' => $coin->quote->USD->percent_change_1h, 'percent_change_24h' => $coin->quote->USD->percent_change_24h, 'percent_change_7d' => $coin->quote->USD->percent_change_7d, 'market_cap' => $coin->quote->USD->market_cap];
+            $coin = (object) ['name' => $coin->name, 'symbol' => $coin->symbol, 'price' => $coin->quote->USD->price, 'price_rls' => ($coin->quote->USD->price * $sellPrice), 'volume_24h' => $coin->quote->USD->volume_24h, 'percent_change_1h' => $coin->quote->USD->percent_change_1h, 'percent_change_24h' => $coin->quote->USD->percent_change_24h, 'percent_change_7d' => $coin->quote->USD->percent_change_7d, 'market_cap' => $coin->quote->USD->market_cap];
         }
 
         return view('home.coins.show', compact('coin'));
+    }
+
+    protected function getUSDTPrice()
+    {
+        $address = [
+            'https://api.nobitex.ir', 'market', 'stats?srcCurrency=usdt&dstCurrency=rls'
+        ];
+
+        $response = \PG\Request\Request::instance()
+            ->setAddress($address)
+            ->getResponse()
+            ->asArray();
+
+        return $response;
     }
 
     protected function getMarketCoins()
