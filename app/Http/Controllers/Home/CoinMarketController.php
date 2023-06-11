39142dc85e00f3c99c7e659ab1ea7a59c644c3bf
Saeed Johari <59645534+persgeek@users.noreply.github.com>
2023-06-11 11:31:48 +0330

    Improve list of coins

--- app/Http/Controllers/Home/CoinMarketController.php
+++ app/Http/Controllers/Home/CoinMarketController.php
@@ -86,11 +86,13 @@ class CoinMarketController extends Controller
 
             $coin = (object) ['name' => $coin->name, 'symbol' => $coin->symbol, 'price' => $coin->quote->USD->price, 'price_ir' => ($coin->quote->USD->price * $lastPrice), 'volume_24h' => $coin->quote->USD->volume_24h, 'percent_change_1h' => $coin->quote->USD->percent_change_1h, 'percent_change_24h' => $coin->quote->USD->percent_change_24h, 'percent_change_7d' => $coin->quote->USD->percent_change_7d, 'market_cap' => $coin->quote->USD->market_cap];
         }
-
+        
+        // Coin info
+        $info = $this->getMarketCoinInfo($symbol);
 
         $articles = Article::orderBy('updated_at', 'desc')->where('is_active', 1)->take(4)->get();
 
-        return view('home.coins.show', compact('coin', 'articles'));
+        return view('home.coins.show', compact('coin', 'articles', 'info'));
     }
 
     protected function getUSDTPrice()
@@ -130,6 +132,25 @@ class CoinMarketController extends Controller
     {
         $address = [
             'https://pro-api.coinmarketcap.com', 'v1', 'cryptocurrency', 'quotes', "latest?symbol={$symbol}"
+        ];
+
+        $headers = [
+            'X-CMC_PRO_API_KEY' => '0af4288d-7634-49c9-9338-8a7798e06d5c'
+        ];
+
+        $response = \PG\Request\Request::instance()
+            ->setAddress($address)
+            ->setHeaders($headers)
+            ->getResponse()
+            ->asObject();
+
+        return $response;
+    }
+    
+    protected function getMarketCoinInfo($symbol)
+    {
+        $address = [
+            'https://pro-api.coinmarketcap.com', 'v2', 'cryptocurrency', "info?symbol={$symbol}"
         ];
 
         $headers = [
