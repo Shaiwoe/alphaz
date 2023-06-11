18dc29bec90d5727fdb734b3958439f4a1aeb569
Mohsen <shaiwoe@gmail.com>
2023-06-11 18:37:21 +0330

    Update ArticleController.php

--- app/Http/Controllers/Home/ArticleController.php
+++ app/Http/Controllers/Home/ArticleController.php
@@ -18,13 +18,13 @@ class ArticleController extends Controller
             ->paginate(9);
 
 
-        $sevenArticle = Article::orderBy('updated_at', 'desc')->where('is_active', 1)->take(7)->skip(1)->latest()->get();
-        $sexArticle = Article::orderBy('updated_at', 'desc')->where('is_active', 1)->take(7)->skip(2)->latest()->get();
-        $fiveArticle = Article::orderBy('updated_at', 'desc')->where('is_active', 1)->take(7)->skip(3)->latest()->get();
-        $forArticle = Article::orderBy('updated_at', 'desc')->where('is_active', 1)->take(7)->skip(4)->latest()->get();
-        $threeArticle = Article::orderBy('updated_at', 'desc')->where('is_active', 1)->take(7)->skip(5)->latest()->get();
-        $twoArticle = Article::orderBy('updated_at', 'desc')->where('is_active', 1)->take(7)->skip(6)->latest()->get();
-        $oneArticle = Article::orderBy('updated_at', 'desc')->where('is_active', 1)->take(7)->skip(7)->latest()->get();
+        $sevenArticle = Article::orderBy('created_at', 'desc')->where('is_active', 1)->take(1)->skip(1)->latest()->get();
+        $sexArticle = Article::orderBy('created_at', 'desc')->where('is_active', 1)->take(1)->skip(2)->latest()->get();
+        $fiveArticle = Article::orderBy('created_at', 'desc')->where('is_active', 1)->take(1)->skip(3)->latest()->get();
+        $forArticle = Article::orderBy('created_at', 'desc')->where('is_active', 1)->take(1)->skip(4)->latest()->get();
+        $threeArticle = Article::orderBy('created_at', 'desc')->where('is_active', 1)->take(1)->skip(5)->latest()->get();
+        $twoArticle = Article::orderBy('created_at', 'desc')->where('is_active', 1)->take(1)->skip(6)->latest()->get();
+        $oneArticle = Article::orderBy('created_at', 'desc')->where('is_active', 1)->take(1)->skip(7)->latest()->get();
 
         return view('home.articles.index', compact('articles', 'sevenArticle', 'sexArticle' , 'forArticle' , 'fiveArticle', 'threeArticle' , 'twoArticle' , 'oneArticle'));
     }
