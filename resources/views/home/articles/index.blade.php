b57fb5b791c31a77d20fcbc56bd9d1bd8dfed2c5
Mohsen <shaiwoe@gmail.com>
2023-06-20 18:10:10 +0330

    Update index.blade.php

--- resources/views/home/articles/index.blade.php
+++ resources/views/home/articles/index.blade.php
@@ -47,12 +47,13 @@
             </div>
         </form>
 
-
+        <?php if (request()->has('search')) {?>
         <div class="flex justify-center items-center w-full mt-12">
             <p class="bg-green flex rounded-2xl p-4 text-base mb-4 items-center gap-2 text-white">
-                 تعداد {{ $articlesCount }} مقاله برای این جستجو پیدا شد
+                تعداد {{ $articlesCount }} مقاله برای این جستجو پیدا شد
             </p>
         </div>
+        <?php }?>
 
 
 
@@ -822,7 +823,6 @@
 
             $('#filter-form').submit();
         }
-
     </script>
 
 </body>
