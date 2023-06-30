8c486e25a214682c0371c1a5e51c097033a894f0
Mohsen <shaiwoe@gmail.com>
2023-06-30 20:53:46 +0330

    Update index.blade.php

--- resources/views/manager/books/index.blade.php
+++ resources/views/manager/books/index.blade.php
@@ -108,7 +108,7 @@
                                             ویرایش
                                         </a>
 
-                                        {{-- <form action="{{ route('books.destroy', ['book' => $book->id]) }}" method="POST">
+                                        <form action="{{ route('books.destroy', ['book' => $book->id]) }}" method="POST">
                                             @csrf
                                             @method('DELETE')
 
@@ -116,7 +116,7 @@
                                                 class="flex bg-red text-white px-4 py-2 rounded-3xl">
                                                 حذف
                                             </button>
-                                        </form> --}}
+                                        </form>
 
                                     </td>
 
