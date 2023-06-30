b5df490be69e7443b0a3cc3e59ed95a7d5feacb9
Mohsen <shaiwoe@gmail.com>
2023-06-30 15:37:34 +0330

    Update nav.blade.php

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -158,7 +158,7 @@
                 <a href="{{ route('maps') }}" class="text-sm text-white my-3 dark:text-zinc-900">1.2.2 ver</a>
 
 
-                @can('Admins')
+                @can('New')
                     <a href="{{ route('managers') }}" class="text-sm text-white my-3 dark:text-zinc-900">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6">
