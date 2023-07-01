6977a62ff84c9d10321f5754dfa69e4d4858ecec
Saeed Johari <59645534+persgeek@users.noreply.github.com>
2023-07-02 02:02:54 +0330

    Update edit.blade.php

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -157,7 +157,6 @@
 
                             <div class="w-full flex justify-end p-12">
                                 <div class="flex flex-col space-y-8 items-center">
-                                    {{ print_r(Auth::user()) }}
                                     <img class="w-6/12 rounded-full" src="{{ Auth::user()->avatar }}" alt="">
                                     <p class="bg-indigo-1 p-2 rounded-lg">{{ Auth::user()->name }} خوش آمدید</p>
                                 </div>
@@ -361,7 +360,7 @@
                         </div>
                      </form>
 <script>
-jQuery(document).ready(function() {
+$(document).ready(function() {
 
 
     $(".avatar_cl").click(function(e) {
