74317799c6b8672f910502123136b8a0d9304aba
Mohsen <shaiwoe@gmail.com>
2023-07-01 08:12:30 +0330

    Update TagController.php

--- app/Http/Controllers/Home/TagController.php
+++ app/Http/Controllers/Home/TagController.php
@@ -12,11 +12,13 @@ class TagController extends Controller
 {
     public function show(Request $request,Tag $tag)
     {
-    
+
         $articles = DB::table('article_tags')->select('article_id')->where('tag_id', $tag->id)->pluck('article_id');
 
         $articles = Article::whereIn('id', $articles)->get();
 
-        return view('home.tags.show' , compact('articles' , 'tag'));
+        $tags = DB::select('select ANY_VALUE(a.title) as title, ANY_VALUE(a.slug) as slug, COUNT(b.tag_id) as total FROM tags a inner join article_tags b ON b.tag_id = a.id group by b.tag_id order by total desc limit 15');
+
+        return view('home.tags.show' , compact('articles' , 'tag' , 'tags'));
     }
 }
