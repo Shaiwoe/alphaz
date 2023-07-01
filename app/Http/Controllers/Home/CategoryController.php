ded28e2f409dfbb7a9d18876172056ddb67b346f
Mohsen <shaiwoe@gmail.com>
2023-07-01 07:03:44 +0330

    Update CategoryController.php

--- app/Http/Controllers/Home/CategoryController.php
+++ app/Http/Controllers/Home/CategoryController.php
@@ -3,20 +3,61 @@
 namespace App\Http\Controllers\Home;
 
 use App\Models\Tag;
+use App\Models\Article;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
-    public function show(Request $request,Category $category)
+    public function show(Request $request, Category $category)
     {
-        $articles = $category->articles()->orderBy('created_at', 'desc')->where('is_active' , 1)->get();
-        $articless = $category->articles()->orderBy('created_at', 'ASC')->where('is_active' , 1)->get();
-        $articlesss = $category->articles()->orderBy('updated_at', 'desc')->where('is_active' , 1)->get();
+        $articles = Article::query()
+            ->orderBy('created_at', 'desc')
+            ->where('is_active', 1)
+            ->search()
+            ->paginate(12);
+
+        $articlesCount = Article::query()
+            ->orderBy('created_at', 'desc')
+            ->where('is_active', 1)
+            ->search()
+            ->count();
+
+
+
+        $articless = Article::query()
+            ->orderBy('created_at', 'ASC')
+            ->where('is_active', 1)
+            ->search()
+            ->paginate(12);
+
+
+        $articlesss = Article::query()
+            ->orderBy('updated_at', 'desc')
+            ->where('is_active', 1)
+            ->search()
+            ->paginate(12);
+
+
+
+        $articleView = Article::query()
+            ->orderBy('viewCount', 'desc')
+            ->where('is_active', 1)
+            ->search()
+            ->paginate(12);
+
+
+        $articleViews = Article::query()
+            ->orderBy('viewCount', 'ASC')
+            ->where('is_active', 1)
+            ->search()
+            ->paginate(12);
+
+        $tags = DB::select('select ANY_VALUE(a.title) as title, ANY_VALUE(a.slug) as slug, COUNT(b.tag_id) as total FROM tags a inner join article_tags b ON b.tag_id = a.id group by b.tag_id order by total desc limit 15');
 
 
         $tags = Tag::all();
-        return view('home.categories.show' , compact('articles', 'articless', 'articlesss' , 'category' , 'tags'));
+        return view('home.categories.show', compact('tags', 'articles', 'articlesCount', 'articless', 'articlesss', 'articleView', 'articleViews'));
     }
 }
