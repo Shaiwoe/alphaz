207378f62921f3090db3118aff015c17f6f1df88
Saeed Johari <59645534+persgeek@users.noreply.github.com>
2023-07-02 02:21:42 +0330

    Update CategoryController.php

--- app/Http/Controllers/Home/CategoryController.php
+++ app/Http/Controllers/Home/CategoryController.php
@@ -57,6 +57,6 @@ class CategoryController extends Controller
 
         $tags = DB::select('select ANY_VALUE(a.title) as title, ANY_VALUE(a.slug) as slug, COUNT(b.tag_id) as total FROM tags a inner join article_tags b ON b.tag_id = a.id group by b.tag_id order by total desc limit 15');
 
-        return view('home.categories.show', compact('tags', 'articles', 'articlesCount', 'articless', 'articlesss', 'articleView', 'articleViews'));
+        return view('home.categories.show', compact('tags', 'category', 'articles', 'articlesCount', 'articless', 'articlesss', 'articleView', 'articleViews'));
     }
 }
