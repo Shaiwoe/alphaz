e5bed97f810c93fae1b393da2289957d0329e902
Mohsen <shaiwoe@gmail.com>
2023-07-01 08:16:18 +0330

    update tag

--- app/Http/Controllers/Home/TagController.php
+++ app/Http/Controllers/Home/TagController.php
@@ -13,55 +13,12 @@ class TagController extends Controller
     public function show(Request $request,Tag $tag)
     {
 
-        $articles = Article::query()
-        ->orderBy('created_at', 'desc')
-        ->where('is_active', 1)
-        ->search()
-        ->paginate(12);
-
-    $articlesCount = Article::query()
-        ->orderBy('created_at', 'desc')
-        ->where('is_active', 1)
-        ->search()
-        ->count();
-
-
-
-    $articless = Article::query()
-        ->orderBy('created_at', 'ASC')
-        ->where('is_active', 1)
-        ->search()
-        ->paginate(12);
-
-
-    $articlesss = Article::query()
-        ->orderBy('updated_at', 'desc')
-        ->where('is_active', 1)
-        ->search()
-        ->paginate(12);
-
-
-
-    $articleView = Article::query()
-        ->orderBy('viewCount', 'desc')
-        ->where('is_active', 1)
-        ->search()
-        ->paginate(12);
-
-
-    $articleViews = Article::query()
-        ->orderBy('viewCount', 'ASC')
-        ->where('is_active', 1)
-        ->search()
-        ->paginate(12);
-
-
         $articles = DB::table('article_tags')->select('article_id')->where('tag_id', $tag->id)->pluck('article_id');
 
         $articles = Article::whereIn('id', $articles)->get();
 
-        $tags = DB::select('select ANY_VALUE(a.title) as title, ANY_VALUE(a.slug) as slug, COUNT(b.tag_id) as total FROM tags a inner join article_tags b ON b.tag_id = a.id group by b.tag_id order by total desc limit 15');
+         $tags = DB::select('select ANY_VALUE(a.title) as title, ANY_VALUE(a.slug) as slug, COUNT(b.tag_id) as total FROM tags a inner join article_tags b ON b.tag_id = a.id group by b.tag_id order by total desc limit 15');
 
-        return view('home.tags.show' , compact('articles' , 'tag' , 'tags' , 'articlesCount', 'articless', 'articlesss', 'articleView', 'articleViews'));
+        return view('home.tags.show' , compact('articles' , 'tag', 'tags'));
     }
 }
