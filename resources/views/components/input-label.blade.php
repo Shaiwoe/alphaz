69a4a20957ee7f98ff9deb1fd73a8813b03b0b18
Mohsen <shaiwoe@gmail.com>
2023-07-02 04:21:51 +0330

    Update input-label.blade.php

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block  text-sm text-gray-400 dark:text-gray-300']) }}>
+<label {{ $attributes->merge(['class' => 'block  text-sm text-gray-400 dark:text-gray-600']) }}>
     {{ $value ?? $slot }}
 </label>
