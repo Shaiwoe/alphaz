431fd012e3f30fd287764f3bbb56bebc0dd205da
Saeed Johari <59645534+persgeek@users.noreply.github.com>
2023-07-03 15:09:49 +0330

    Update show.blade.php

--- resources/views/home/categories/show.blade.php
+++ resources/views/home/categories/show.blade.php
@@ -130,43 +130,7 @@
                             </div>
                         @endforeach}}
 
-                        <!--@php
-                            $parentCategorys = App\Models\Category::where('parent_id', 0)->get();
-                        @endphp
-
-                        @foreach ($parentCategorys as $parentCategory)
-                            <div class="sidenav p-1 m-2 z-20">
-                                <button
-                                    class="dropdown-btn hover:bg-green rounded-full flex py-2 px-3 dark:bg-slate-300">
-                                    <svg class="w-3 h-3 self-center ml-2" viewBox="0 0 14.828 8.414">
-                                        <path class="dark:stroke-zinc-900" id="chevron-right" d="M9,18l6-6L9,6"
-                                            transform="translate(19.414 -7.586) rotate(90)" fill="none"
-                                            stroke="#fff" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="2" />
-                                    </svg>
-
-                                    {{ $parentCategory->title }}
-
-                                </button>
-                                <div class="dropdown-container z-20 mr-5">
-
-                                    @foreach ($parentCategory->children as $childCategory)
-                                        <a class="flex mb-3"
-                                            href="{{ route('home.categories.show', ['category' => $childCategory->slug]) }}">
-                                            <svg class="w-3 h-3 self-center ml-2" viewBox="0 0 8 8">
-                                                <circle class="dark:fill-zinc-900" id="Ellipse_241"
-                                                    data-name="Ellipse 241" cx="4" cy="4"
-                                                    r="4" fill="#fff" />
-                                            </svg>
-
-                                            {{ $childCategory->title }}
-
-                                        </a>
-                                    @endforeach
-
-                                </div>
-                            </div>
-                        @endforeach-->
+                        
 
                     </div>
 
