ccc8a64b32ac6b13dc689c7ae4f2504b55ff2368
Saeed Johari <59645534+persgeek@users.noreply.github.com>
2023-07-04 16:00:24 +0330

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,7 +29,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'avatar',
         'password',
         'email_verified_at',
-        'wallet_bit', 'wallet_eth', 'wallet_usdt'
+        'wallet_bit', 'wallet_eth', 'wallet_usdt', 'google'
     ];
 
     /**
