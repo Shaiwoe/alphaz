23caf6eb0f7a60d9f3b90f47d644710224ec7a71
Saeed Johari <59645534+persgeek@users.noreply.github.com>
2023-07-04 16:01:36 +0330

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        'google' => \PragmaRX\Google2FALaravel\Middleware::class,
     ];
 
     /**
@@ -67,5 +66,6 @@ class Kernel extends HttpKernel
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+        'google' => \PragmaRX\Google2FALaravel\Middleware::class,
     ];
 }
