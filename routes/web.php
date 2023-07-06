5e08d6a3c572c844aff44c460988538a13c342a4
Mohsen <shaiwoe@gmail.com>
2023-07-06 15:56:44 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -205,13 +205,13 @@ Route::get('/question', [QuestionController::class, 'index'])->middleware(['auth
 Route::get('/wishlist', [WishlistController::class, 'userProfile'])->middleware(['auth', 'verified' , 'google'])->name('wishlist');
 Route::get('/webinar', [WebinarController::class, 'show'])->middleware(['auth', 'verified' , 'google'])->name('webinar');
 
-Route::get('/profile', [ProfileController::class, 'edit'])->middleware(['auth'])->name('profile.edit');
-Route::patch('/profile', [ProfileController::class, 'update'])->middleware(['auth'])->name('profile.update');
-Route::delete('/profile', [ProfileController::class, 'destroy'])->middleware(['auth', 'verified'])->name('profile.destroy');
-Route::patch('/profile/avatar', [ProfileController::class, 'avatar'])->middleware(['auth'])->name('profile.avatar');
-Route::patch('/profile/wallet', [ProfileController::class, 'wallet'])->middleware(['auth'])->name('profile.wallet');
-Route::get('/profile/google', [ProfileController::class, 'google'])->middleware(['auth'])->name('profile.google');
-Route::post('/profile/accept', [ProfileController::class, 'accept'])->middleware(['auth'])->name('profile.accept');
+Route::get('/profile', [ProfileController::class, 'edit'])->middleware(['auth' , 'google'])->name('profile.edit');
+Route::patch('/profile', [ProfileController::class, 'update'])->middleware(['auth' , 'google'])->name('profile.update');
+Route::delete('/profile', [ProfileController::class, 'destroy'])->middleware(['auth', 'verified' , 'google'])->name('profile.destroy');
+Route::patch('/profile/avatar', [ProfileController::class, 'avatar'])->middleware(['auth' , 'google'])->name('profile.avatar');
+Route::patch('/profile/wallet', [ProfileController::class, 'wallet'])->middleware(['auth' , 'google'])->name('profile.wallet');
+Route::get('/profile/google', [ProfileController::class, 'google'])->middleware(['auth' , 'google'])->name('profile.google');
+Route::post('/profile/accept', [ProfileController::class, 'accept'])->middleware(['auth' , 'google'])->name('profile.accept');
 
 
 
