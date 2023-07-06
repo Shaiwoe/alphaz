7b4530df00f77d6f1b204316301564762d801df6
Saeed Johari <59645534+persgeek@users.noreply.github.com>
2023-07-06 14:26:34 +0330

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -45,7 +45,7 @@ class ProfileController extends Controller
         }
 
         // Get google from session
-        $google = request()->session()->get('google');
+        $google = request()->session()->pull('google', null);
 
         if (empty($google)) {
             return redirect()->route('profile.edit');
