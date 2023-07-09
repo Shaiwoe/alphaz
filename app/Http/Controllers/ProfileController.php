503db6d37eea3562caeff427adcc4676587a2e2d
Mohsen <shaiwoe@gmail.com>
2023-07-09 13:52:58 +0330

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -71,7 +71,7 @@ class ProfileController extends Controller
         $user->save();
 
         return redirect()->route('profile.edit');
-        alert()->info('با موفقیت غیرفعال شد', 'با تشکر');
+        alert()->success('با موفقیت غیرفعال شد', 'با تشکر');
 
     }
 
